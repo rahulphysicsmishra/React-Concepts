28e84fd9079a8447d1d1ed60e64b6f5e9283b91d
rahul mishra <rahulphysicsmishra@gmail.com>
2025-01-21 18:37:47 +0530

    more practice of hoc

--- hoc-test/src/App.jsx
+++ hoc-test/src/App.jsx
@@ -16,14 +16,58 @@ const HelloComponent = ({name, age}) => {
   )
 }
 
+
 const EnhancedHelloComponent = withLogger(HelloComponent);
+
+
+const withLoggerNew = (WrappedComponent) => {
+  return (props) => {
+    console.log("New props : ", props);
+    return <WrappedComponent {...props} />
+  };
+}
+
+const UserInfo = ({name, age, contact}) => {
+  return (
+    <div>
+      <h1>Hello, {name}</h1>
+      <p>Your age is {age}</p>
+      <p>Your contact is {contact}</p>
+    </div>
+  )
+}
+
+const EnhancedHelloComponentNew = withLoggerNew(UserInfo);
+
+const withStatus = (WrappedComponent) => {
+  return (props) => {
+    const status = (props.age)>=18 ?  "Adult" : "Minor" ;
+    const canVote = (props.citizen) == 1 ? "yes" : "no";
+    console.log("All props: ", props);
+    return <WrappedComponent {...props} status={status} canVote={canVote} />
+  }
+}
+
+const enhancedUserInfo = ({name, age, status, citizen, canVote}) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{age}</p>
+      <p>{status}</p>
+      <p>Can vote : {canVote}</p>
+    </div>
+  )
+}
+const EnhancedHelloComponent2 = withStatus(enhancedUserInfo);
 
 function App() {
 
   return (
-    <>
+    <div>
       <EnhancedHelloComponent name="Rahul" age={32} />
-    </>
+      <EnhancedHelloComponentNew name="Bhuvi" age={32} contact={1234} />
+      <EnhancedHelloComponent2 name="Andy" age={36} citizen={1} />
+    </div>
   )
 }
 
