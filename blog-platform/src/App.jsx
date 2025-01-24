2bcad0ab6e40f1cb5894861427af47c275acd4cb
rahul mishra <rahulphysicsmishra@gmail.com>
2025-01-25 05:06:05 +0530

    Updated App.jsx and added components and pages

--- blog-platform/src/App.jsx
+++ blog-platform/src/App.jsx
@@ -1,11 +1,18 @@
-
+import Navbar from "./components/Navbar"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./pages/Home";
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
